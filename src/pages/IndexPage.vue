2b76255d881edd653bbf2d4a77530d8a94d4c2cd
Aarón Negrín <demenus.dev@gmail.com>
2025-07-12 10:52:39 +0000

    feat: Adding a logo

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,43 +1,17 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="flex column items-center justify-start q-pa-md">
+    <q-img src="/mimic-chest.png" alt="Mimic Chest" class="mimic-chest-image" />
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import type { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1',
-  },
-  {
-    id: 2,
-    content: 'ct2',
-  },
-  {
-    id: 3,
-    content: 'ct3',
-  },
-  {
-    id: 4,
-    content: 'ct4',
-  },
-  {
-    id: 5,
-    content: 'ct5',
-  },
-]);
-
-const meta = ref<Meta>({
-  totalCount: 1200,
-});
+//
 </script>
+
+<style scoped>
+.mimic-chest-image {
+  max-width: 100%;
+  height: auto;
+  margin-top: 2rem;
+}
+</style>
